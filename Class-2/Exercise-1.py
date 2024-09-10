b602584f8d5677b6c0151419f2329cc2691863d7
Azhar Rizki Zulma <azharrizki@zulma.id>
2024-09-10 10:54:46 +0700

    Create Exercise-1.py

---
+++ Class-2/Exercise-1.py
@@ -0,0 +1,7 @@
+g_flt_length = float(input("Masukkan Panjang: "))
+g_flt_wide = float(input("Masukkan Lebar: "))
+g_flt_height = float(input("Masukkan Tinggi: "))
+
+g_flt_vol = g_flt_length * g_flt_wide * g_flt_height
+
+print("Luas Volume dari Balok adalah " + str(g_flt_vol) + " m³")
