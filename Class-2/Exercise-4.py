98c00c4d4496804a440b880a74494392bca65eee
Azhar Rizki Zulma <azharrizki@zulma.id>
2024-09-10 11:01:51 +0700

    Create Exercise-4.py

---
+++ Class-2/Exercise-4.py
@@ -0,0 +1,7 @@
+PI = 3.14159265358979323846
+
+g_int_radius = float(input("Masukkan Jari-jari: "))
+
+g_flt_kel = (4 * PI * (g_int_radius * g_int_radius))/3
+
+print("Luas Keliling dari Bola adalah " + str(g_flt_vol))
