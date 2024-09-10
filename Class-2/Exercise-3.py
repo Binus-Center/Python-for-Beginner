2a355a99d05ad9bfd6532f08a5f296c7088e4a42
Azhar Rizki Zulma <azharrizki@zulma.id>
2024-09-10 11:02:10 +0700

    Update Exercise-3.py

--- Class-2/Exercise-3.py
+++ Class-2/Exercise-3.py
@@ -4,4 +4,4 @@ g_int_radius = float(input("Masukkan Jari-jari: "))
 
 g_flt_vol = (4 * PI * (g_int_radius * g_int_radius * g_int_radius))/3
 
-print("Luas Volume dari Tabung adalah " + str(g_flt_vol) + " m³")
+print("Luas Volume dari Bola adalah " + str(g_flt_vol) + " m³")
